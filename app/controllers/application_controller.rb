f7e653a2dd16ac0cca80790cceb983f2c699851e
Alie Stone <helloaliestone@gmail.com>
2020-11-29 17:54:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,12 @@ class App < Sinatra::Base
       erb :super_hero
     end
 
-    post "teams" do
-      erb :team
-    end
+    post "/teams" do
+     @team_name = params["team"]["name"]
+     @team_motto = params["team"]["motto"]
+     @team_members = params["team"]["members"]
+
+     erb :team
+   end
 
 end
